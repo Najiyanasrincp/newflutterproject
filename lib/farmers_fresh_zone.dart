46400d5214e9c227f2c445c3caf8ba37b78e89d6
LENOVO <najiyanasrincp@gmail.com>
2023-11-06 23:44:39 -0800

    Commit

--- lib/farmers_fresh_zone.dart
+++ lib/farmers_fresh_zone.dart
@@ -2,7 +2,6 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'carousel.dart';
 
 void main() {
